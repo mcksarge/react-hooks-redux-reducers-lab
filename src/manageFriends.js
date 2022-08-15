9f11a859f9d1405eadf3861e7693e23424143765
Fred Higgins <higgins.fred.s@gmail.com>
2022-08-15 13:50:49 -0700

    commit tree e06af12afb1302b69f6758a076a55bdfd433c14e

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,18 @@
 export function manageFriends(state, action) {
-  // your code here
+  switch (action.type) {
+    case "friends/add":
+      return {
+        ...state,
+        friends: [...state.friends, action.payload]
+      }
+      case "friends/remove":
+        console.log(action.payload)
+        const friends = state.friends.filter(friend => friend.id !== action.payload)
+        console.log(friends)
+        return {
+          friends
+        }
+    default:
+      return state;
+  }
 }
